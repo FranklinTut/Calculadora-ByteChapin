04900d324255839e06d59bf5be33b027c96d3799
Francin <ftutc@miumg.edu.gt>
2025-08-02 23:14:30 -0600

    :egg: Agregue la funcion de Division 1

--- Program.cs
+++ Program.cs
@@ -12,8 +12,70 @@ class Program
         Console.Write("Seleccione una opción: ");
 
         int opcion = int.Parse(Console.ReadLine());
-        // TODO: Implementar la lógica de menú
+        // Implementar la lógica de menú
+        switch (opcion)
+        {
+            case 1:
+                Console.Write("Ingrese el primer número: ");
+                double sumaNum1 = double.Parse(Console.ReadLine());
+                Console.Write("Ingrese el segundo número: ");
+                double sumaNum2 = double.Parse(Console.ReadLine());
+                double sumaResultado = Sumar(sumaNum1, sumaNum2);
+                Console.WriteLine($"El resultado de la suma es: {sumaResultado}");
+                break;
+            case 2:
+                Console.Write("Ingrese el primer número: ");
+                double restaNum1 = double.Parse(Console.ReadLine());
+                Console.Write("Ingrese el segundo número: ");
+                double restaNum2 = double.Parse(Console.ReadLine());
+                double restaResultado = Restar(restaNum1, restaNum2);
+                Console.WriteLine($"El resultado de la resta es: {restaResultado}");
+                break;
+            case 3:
+                Console.Write("Ingrese el primer número: ");
+                double multiNum1 = double.Parse(Console.ReadLine());
+                Console.Write("Ingrese el segundo número: ");
+                double multiNum2 = double.Parse(Console.ReadLine());
+                double multiResultado = Multiplicar(multiNum1, multiNum2);
+                Console.WriteLine($"El resultado de la multiplicación es: {multiResultado}");
+                break;
+            case 4:
+                Console.Write("Ingrese el primer número: ");
+                double num1 = double.Parse(Console.ReadLine());
+                Console.Write("Ingrese el segundo número: ");
+                double num2 = double.Parse(Console.ReadLine());
+                double resultado = Dividir(num1, num2);
+                Console.WriteLine($"El resultado de la división es: {resultado}");
+                break;
+            default:
+                Console.WriteLine("Opción no válida.");
+                break;
+        }
     }
 
-    // TODO: Implementar funciones de suma, resta, multiplicación, división
+    // Implementar funciones de suma, resta, multiplicación, división
+    static double Sumar(double a, double b)
+    {
+        return a + b;
+    }
+
+    static double Restar(double a, double b)
+    {
+        return a - b;
+    }
+
+    static double Multiplicar(double a, double b)
+    {
+        return a * b;
+    }
+
+    static double Dividir(double a, double b)
+    {
+        if (b == 0)
+        {
+            Console.WriteLine("No se puede dividir por cero.");
+            return double.NaN;
+        }
+        return a / b;
+    }
 }
--- obj/Debug/net9.0/lab001.AssemblyInfo.cs
+++ obj/Debug/net9.0/lab001.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab001")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94f05c1f5e17e59e3fb1fa68eb8a78296275df3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab001")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab001")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
